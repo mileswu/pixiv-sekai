2ac21042cfcdc9ceffa772ed18cef642240b69e9
Miles Wu <miles.wu@wu-home.co.uk>
2015-08-21 23:37:59 -0500

    LoginPage hooked up to login
    - Created PixivAPI class
    - Stored as a property on the App
    - Login returns a Task<bool> now for success
    - LoginPage navigates to MainPage upon login success

--- pixiv-sekai/LoginPageModel.cs
+++ pixiv-sekai/LoginPageModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace pixiv_sekai
 {
@@ -23,10 +25,15 @@ namespace pixiv_sekai
             LoginCommand = new SimpleRelayCommand(Login);
         }
 
-        private void Login(object o)
+        private async void Login(object o)
         {
-            Debug.WriteLine("Username = " + Username);
-            Debug.WriteLine("Password = " + Password);
+            Task<bool> loginTask = (App.Current as App).Pixiv.Login(Username, Password);
+            bool loginSuccessful = await loginTask;
+            if (loginSuccessful)
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                rootFrame.Navigate(typeof(MainPage));
+            }
         }
 
     }
---
+++ pixiv-sekai/PixivAPI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+
+namespace pixiv_sekai
+{
+    public class PixivAPI
+    {
+        async public Task<bool> Login(string username, string password)
+        {
+            WebRequest webRequest = WebRequest.Create("https://oauth.secure.pixiv.net/auth/token");
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            string postDataString = "username=" + username + "&password=" + password + "&grant_type=password&client_id=bYGKuGVw91e0NMfPGp44euvGt59s&client_secret=HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK";
+            byte[] postDataBytes = Encoding.UTF8.GetBytes(postDataString);
+            using (Stream newStream = await webRequest.GetRequestStreamAsync())
+            {
+                await newStream.WriteAsync(postDataBytes, 0, postDataBytes.Length);
+            }
+
+            string responseBody;
+            try
+            {
+                WebResponse webResponse = await webRequest.GetResponseAsync();
+                responseBody = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+            }
+            catch (WebException exception)
+            {
+                WebResponse webResponse = exception.Response;
+                responseBody = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+                Debug.WriteLine("ERROR");
+                Debug.WriteLine(responseBody);
+                return false;
+            }
+
+            JObject responseJSON = JObject.Parse(responseBody);
+
+            string token = (string)responseJSON["response"]["access_token"];
+            int lifetime = (int)responseJSON["response"]["expires_in"];
+            Debug.WriteLine("Obtained token (token = " + token + ", lifetime = " + lifetime + ")");
+            return true;
+        }
+    }
+}
