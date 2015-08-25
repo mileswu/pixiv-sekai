d5e332d71a27897e961d3ded1b5ed28ba61a5632
Miles Wu <miles.wu@wu-home.co.uk>
2015-08-24 22:20:32 -0500

    cdsds

--- pixiv-sekai/App.xaml.cs
+++ pixiv-sekai/App.xaml.cs
@@ -48,7 +48,7 @@ namespace pixiv_sekai
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
