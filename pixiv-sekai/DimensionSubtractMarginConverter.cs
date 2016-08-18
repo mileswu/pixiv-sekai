ad94ccac1ecbeb8af1ae7a6140c62103411954e7
Miles Wu <miles.wu@wu-home.co.uk>
2016-08-18 11:29:45 -0500

    RankingPage: fix padding/margins of GridView
    Items are now properly centered!
    GridViewItem has zero padding and a margin of 8
    Need to adjust Grid in ItemTemplate to have a width of ItemWidth-2*8, so
    we use a binding and a converter

---
+++ pixiv-sekai/DimensionSubtractMarginConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace pixiv_sekai
+{
+    class DimensionSubtractMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (double)value - 2.0*System.Convert.ToDouble((string)parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (double)value + 2.0*System.Convert.ToDouble((string)parameter);
+        }
+    }
+}
